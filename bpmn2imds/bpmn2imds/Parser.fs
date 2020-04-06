1338a36767940279682062c2ad9598c373950ba4
Jakub Jalowiec <01103622@pw.edu.pl>
2020-04-06 19:35:22 +0200

    Started introducing seq {...} comprehensions in BPMN parser.

--- bpmn2imds/bpmn2imds/Parser.fs
+++ bpmn2imds/bpmn2imds/Parser.fs
@@ -39,41 +39,41 @@ module parser =
     // but in the <...> part which is saved in e.ElementRef which is the id of the BPMN element
     // that is represented by drawing of the edge
     let getSeqAndMesEdges (model: BPMN.Model) = 
-        model.Diagrams 
-        |> Seq.collect(fun e -> e.Planes)
-        |> Seq.collect(fun e -> e.Edges)
-        |> Seq.map(fun e -> 
-            if isNull e.ElementRef then
-                Error (ShapeElementRefNull e.ID)
-            else if isNull e.Points then
-                Error (ShapePointsNull e.ID)
-            else if e.Points.Count < 2 then
-                Error (ShapePointsCountLessThanOne e.ID)
-            else Ok (e.ElementRef, Seq.head e.Points, Seq.last e.Points))
-        |> Seq.map(fun e -> 
-            e |> Result.map(fun (elementRef, s, e) -> Edge (elementRef, Start (s.X, s.Y), End (e.X, e.Y))))
-    
+        seq {
+            for e in model.Diagrams 
+                |> Seq.collect(fun e -> e.Planes) 
+                |> Seq.collect(fun e -> e.Edges) ->
+            let edge = 
+                if isNull e.ElementRef then
+                    Error (ShapeElementRefNull e.ID)
+                else if isNull e.Points then
+                    Error (ShapePointsNull e.ID)
+                else if e.Points.Count < 2 then
+                    Error (ShapePointsCountLessThanOne e.ID)
+                else Ok (e.ElementRef, Seq.head e.Points, Seq.last e.Points)
+            edge |> Result.map(fun (elementRef, s, e) -> Edge (elementRef, Start (s.X, s.Y), End (e.X, e.Y)))
+        }
     let getShapes (model: BPMN.Model) = 
-        model.Diagrams 
-        |> Seq.collect(fun e -> e.Planes)
-        |> Seq.collect(fun e -> e.Shapes)
-        |> Seq.map(fun e -> 
-            if isNull e.ElementRef then
-                Error (ShapeElementRefNull e.ID)
-            else if isNull e.Bounds then
-                Error (ShapePointsNull e.ID)
-            else if e.Bounds.Count <> 1 then
-                Ok ((e.ElementRef, Seq.head e.Bounds), [ShapeBoundsCountNotEqualOne e.ID])
-            else Ok ((e.ElementRef, Seq.head e.Bounds), []))
-        |> Seq.map(fun e -> 
-            e |> Result.map(fun ((elementRef, rectangle), warns) -> 
-                ((elementRef, rectangle.Left + rectangle.Width/2, rectangle.Top + rectangle.Height/2), warns)))
-        |> Seq.map(fun e -> 
-            e |> Result.map(fun ((elementRef, X, Y), warns) -> 
-                (Shape (elementRef, Middle (X, Y)), warns)))
+        seq {
+            for s in model.Diagrams 
+                |> Seq.collect(fun e -> e.Planes)
+                |> Seq.collect(fun e -> e.Shapes) ->
+            let tryShape = 
+                if isNull s.ElementRef then
+                    Error (ShapeElementRefNull s.ID)
+                else if isNull s.Bounds then
+                    Error (ShapePointsNull s.ID)
+                else if s.Bounds.Count <> 1 then
+                    Ok ((s.ElementRef, Seq.head s.Bounds), [ShapeBoundsCountNotEqualOne s.ID])
+                else Ok ((s.ElementRef, Seq.head s.Bounds), [])
+            tryShape
+                |> Result.map(fun ((elementRef, rectangle), warns) -> 
+                    ((elementRef, rectangle.Left + rectangle.Width/2, rectangle.Top + rectangle.Height/2), warns))
+                |> Result.map(fun ((elementRef, X, Y), warns) -> 
+                        (Shape (elementRef, Middle (X, Y)), warns))
+        }
 
     let parse (model: BPMN.Model) = 
-        let edges = getSeqAndMesEdges model
         let (shapes, shapeErrors, shapeWarnings) =
             splitResults $ (getShapes model)
         let tryElements = 
@@ -126,7 +126,8 @@ module parser =
         
         let (boundaryFlows, boundaryFlowsErrors, boundaryFlowsWarns) = 
             splitResults tryBoundaryFlows
-
+            
+        let edges = getSeqAndMesEdges model
         let tryFlows = 
             edges |> Seq.map (fun e -> 
                 e |> Result.bind (fun (Edge (elementRef, left, right)) ->
