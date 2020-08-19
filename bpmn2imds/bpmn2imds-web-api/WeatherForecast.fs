2f8c6dd6f98639d5b15575643178084dec695d9b
Jakub Jalowiec <kuba.jalowiec@protonmail.com>
2020-08-19 19:48:01 +0200

    Adding Angular frontend and .NET WebAPI F# backend.

---
+++ bpmn2imds/bpmn2imds-web-api/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace bpmn2imds_web_api
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32 + (int (float this.TemperatureC / 0.5556))
