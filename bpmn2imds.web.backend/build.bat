13f0a9f597da211ed592d0cb43e86187c7000854
Jakub Jalowiec <kuba.jalowiec@protonmail.com>
2020-08-22 23:15:19 +0200

    Replacing dots in directory names with dashes. Adding Angular client in bpmn2imds-web-ui.

--- bpmn2imds.web.backend/build.bat
+++
@@ -1,3 +0,0 @@
-dotnet restore bpmn2imds.web.backend.fsproj
-dotnet build bpmn2imds.web.backend.fsproj
-
--- bpmn2imds.web.backend/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dotnet restore bpmn2imds.web.backend.fsproj
-dotnet build bpmn2imds.web.backend.fsproj
-
