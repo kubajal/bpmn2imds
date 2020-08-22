b44dddcefe13eb0737596982be8b5a9f7632a429
Jakub Jalowiec <kuba.jalowiec@protonmail.com>
2020-08-22 20:36:46 +0200

    Adding C# WebAPI.

---
+++ bpmn2imds.web.api/Controllers/VerificationController.cs
@@ -0,0 +1,59 @@
+﻿using bpmn2imds;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace bpmn2imds.web.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VerificationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("validate")]
+        // to do: replace List<Object> with List<Deadlock>
+        public IActionResult Validate([FromBody] RequestPayload Payload)
+        {
+            int queuedAgents = Payload.queuedAgents;
+            if (queuedAgents > 3)
+                return new ObjectResult("Cannot queue more than 3 agents on AND splits") 
+                {
+                    StatusCode = 400
+                };
+            if (queuedAgents <= 0)
+                return new ObjectResult("Queued agents count must be positive") 
+                {
+                    StatusCode = 400
+                };
+            if (Payload == null)
+            {
+                return new ObjectResult("Payload was null") 
+                {
+                    StatusCode = 400
+                };
+            }
+            string XML = Payload.xml;
+            if (XML == null)
+            {
+                return new ObjectResult("XML was null") 
+                {
+                    StatusCode = 400
+                };
+            }
+
+            // to do: implement diagram validation
+            
+            return new ObjectResult("Processed...") 
+            {
+                StatusCode = 200
+            };
+        }
+    }
+}
+
---
+++ bpmn2imds.web.api/Models/RequestPayload.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bpmn2imds.web.api.Controllers
+{
+    public class RequestPayload
+    {
+        [Required]
+        public string xml { get; set; }
+
+        [Required]
+        public int queuedAgents { get; set; }
+
+        public RequestPayload(string xml)
+        {
+            this.xml = xml;
+        }
+        public RequestPayload() { }
+    }
+}
