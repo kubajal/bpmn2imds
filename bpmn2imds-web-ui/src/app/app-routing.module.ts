700d071ddafd1bc711babd3c6c5937839c9495a0
Jakub Jalowiec <kuba.jalowiec@protonmail.com>
2020-08-28 21:49:20 +0200

    Adding footer, creating placeholders for 'help' & 'about'.

--- bpmn2imds-web-ui/src/app/app-routing.module.ts
+++ bpmn2imds-web-ui/src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
-  { path: '', component: IndexComponent }
+  { path: '', component: IndexComponent },
+  { path: 'index', component: IndexComponent },
+  { path: 'about', component: IndexComponent },
+  { path: 'help', component: IndexComponent }
 ];
 
 @NgModule({
--- bpmn2imds-web-ui/src/app/app.component.ts
+++ bpmn2imds-web-ui/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bpmn2imds-web-ui';
+
+  addr = "enable js to see my mail";
+
+  ngOnInit(): void {
+    this.addr = ["kuba", ".", "jalowiec", "@", "protonmail", ".", "com"].join("");
+  }
 }
